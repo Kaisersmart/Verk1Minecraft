dc5663ba4cd489493f640c2b3100fed1f1c565d6
KaiserSmart <tretakov.e2015@gmail.com>
2026-04-26 17:29:19 +0000

    Node.js + Express + ejs setup with scss

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "src", "views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Mobs Minecraft" });
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server kyrir á http://localhost:${PORT}`);
+})
+
